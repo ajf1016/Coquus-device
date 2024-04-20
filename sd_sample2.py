09bd375f9b997ba006da5941ec189da52780663b
ajf1016 <83690916+ajf1016@users.noreply.github.com>
2024-04-21 00:40:35 +0530

    sd card updated

--- sd_sample2.py
+++ sd_sample2.py
@@ -1,11 +1,29 @@
-import machine
+import os
+from machine import Pin, SoftSPI
 from sdcard import SDCard
-from machine import SoftSPI, Pin
 
-# Initialize SPI
+
+# Initialize SPI and SD card
 spisd = SoftSPI(-1, miso=Pin(13), mosi=Pin(12), sck=Pin(14))
 sd = SDCard(spisd, Pin(27))
 
-# Mount SD card
+print('Root dir before mounting: {}'.format(os.listdir()))
 vfs = os.VfsFat(sd)
-os.mount(vfs, '/sd')
+
+try:
+    os.mount(vfs, '/sd')
+except OSError as e:
+    print("Error mounting SD card:", e)
+
+print('Root dir after mounting: {}'.format(os.listdir('/sd')))
+os.chdir('/sd')
+print('SD card contains: {}'.format(os.listdir()))
+
+# Create a sample file
+with open('/sd/sample.txt', 'w') as f:
+    f.write("This is a sample file.")
+
+# Read the sample file
+with open('/sd/sample.txt', 'r') as f:
+    content = f.read()
+    print("Content of sample.txt:", content)
